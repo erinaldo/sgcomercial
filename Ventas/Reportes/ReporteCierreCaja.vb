3ab193edfe1abd5fb3353af8f93acc99140a07be
lucasmartinbs <lucasmartinbs@gmail.com>
2019-01-13 00:43:12 -0300

    Ajuste reporte cierre caja

--- Ventas/Reportes/ReporteCierreCaja.vb
+++ Ventas/Reportes/ReporteCierreCaja.vb
@@ -1,26 +1,17 @@
 ﻿Public Class ReporteCierreCaja
     Private Sub ReporteCierreCaja_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.cajaresumen' Puede moverla o quitarla según sea necesario.
-        Me.cajaresumenTableAdapter.Fill(Me.comercialDataSet.cajaresumen)
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.librodiario' Puede moverla o quitarla según sea necesario.
-        Me.librodiarioTableAdapter.Fill(Me.comercialDataSet.librodiario)
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.cajaresumen' Puede moverla o quitarla según sea necesario.
-        'Me.cajaresumenTableAdapter.FillByidevento(Me.comercialDataSet.cajaresumen, gidevento)
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.cajaseventos' Puede moverla o quitarla según sea necesario.
-        Me.CajaseventosTableAdapter.Fill(Me.comercialDataSet.cajaseventos)
-        Me.librodiarioTableAdapter.FillByActivos(Me.comercialDataSet.librodiario)
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.MiComercio' Puede moverla o quitarla según sea necesario.
-        Me.MiComercioTableAdapter.Fill(Me.comercialDataSet.MiComercio)
-        'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.librodiario' Puede moverla o quitarla según sea necesario.
 
-        CajaseventosBindingSource.Filter = "idevento = " + gidevento.ToString
-        librodiarioBindingSource.Filter = "idevento = " + gidevento.ToString
+        Me.MiComercioTableAdapter.Fill(Me.comercialDataSet.MiComercio)
+
+        Me.CajaseventosTableAdapter.FillByIdevento(Me.comercialDataSet.cajaseventos, gidevento)
+        Me.librodiarioTableAdapter.FillByIdevento(Me.comercialDataSet.librodiario, gidevento)
+        Me.cajaresumenTableAdapter.FillByidevento(Me.comercialDataSet.cajaresumen, gidevento)
 
         Me.ReportViewer1.RefreshReport()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs)
-        Me.librodiarioTableAdapter.Fill(Me.comercialDataSet.librodiario)
+        Me.librodiarioTableAdapter.FillByIdevento(Me.comercialDataSet.librodiario, gidevento)
         'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.MiComercio' Puede moverla o quitarla según sea necesario.
         Me.MiComercioTableAdapter.Fill(Me.comercialDataSet.MiComercio)
         'TODO: esta línea de código carga datos en la tabla 'comercialDataSet.librodiario' Puede moverla o quitarla según sea necesario.
