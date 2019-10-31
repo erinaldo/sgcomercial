3d22da78a364ea19ac9bb539c3cf33b70f77ebe3
lucasmartinbs <lucasmartinbs@gmail.com>
2019-10-31 10:04:29 -0300

    44

--- MiFranquicia/ReporteVentasFranquicia.vb
+++ MiFranquicia/ReporteVentasFranquicia.vb
@@ -14,9 +14,9 @@
             v_ListaRefFranquicia = ParametrosgeneralesTableAdapter.parametrosgenerales_getprgvalor1byclave("ListaRefFranquicia")
             ''**********
             If v_ListaRefFranquicia = 3 Then
+                Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "sgcomercial.RepVentasFranquiciaLista3.rdlc"
                 Dim msg As String = ""
                 Try
-
                     Me.Cursor = Cursors.WaitCursor
                     Dim desde As Date '= Nothing
                     Dim hasta As Date '= Nothing
@@ -42,7 +42,33 @@
                 End Try
             End If
             If v_ListaRefFranquicia = 4 Then
-                MessageBox.Show("La lista de precios configurada no esta habilitada para esta Franquicia: Reporte no disponible", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "sgcomercial.RepVentasFranquiciaLista4.rdlc"
+                'Me.ReportViewer1.RefreshReport()
+                Dim msg As String = ""
+                Try
+                    Me.Cursor = Cursors.WaitCursor
+                    Dim desde As Date '= Nothing
+                    Dim hasta As Date '= Nothing
+                    msg = " Point A"
+
+                    desde = Convert.ToDateTime(DateTimePickerDesde.Value)
+                    hasta = Convert.ToDateTime(DateTimePickerHasta.Value)
+                    msg = " Point B "
+                    If desde <> Nothing And hasta <> Nothing Then
+                        Me.libroventasTableAdapter.FillByRangoFechas(Me.comercialDataSet.libroventas, desde.ToString, hasta.ToString)
+                        msg = " Point LV "
+                        Me.libroventasdetalleTableAdapter.FillByRangoFechas(Me.comercialDataSet.libroventasdetalle, desde.ToString, hasta.ToString)
+                        msg = " Point LVD "
+                        Me.ReportViewer1.RefreshReport()
+                        Me.Cursor = Cursors.Default
+                    Else
+                        Me.Cursor = Cursors.Default
+                        MsgBox("Seleccione un rango de fechas!", "Advertencia", MsgBoxStyle.Exclamation)
+                    End If
+                Catch ex As Exception
+                    Me.Cursor = Cursors.Default
+                    MsgBox("Ocurrió una excepción al consultar los datos: ", msg + ex.Message)
+                End Try
             End If
         Catch ex As Exception
             MessageBox.Show("Ocurrió una excepción: el reporte no esta disponible -> " + ex.Message, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
